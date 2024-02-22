cb1356ed1498aa331e99124c0adc6c36d0bed965
abdulrahman-02 <abderrahmanebenaissa740@gmail.com>
2024-02-22 20:48:57 +0100

    Add code to display pi in different formats

---
+++ Intro/problem2.m
@@ -0,0 +1,26 @@
+% Display the value of pi
+pi;
+
+% Change the display format to long
+format long;
+
+% Display the value of pi in long format
+pi;
+
+% Change the display format to short scientific notation
+format short e;
+
+% Display the value of pi in short scientific notation
+pi;
+
+% Change the display format to long scientific notation
+format long e;
+
+% Display the value of pi in long scientific notation
+pi;
+
+% Change the display format to bank format
+format bank;
+
+% Display the value of pi in bank format
+pi;
