90dd78526065021aee23f827008d5b8f95c26f62
abdulrahman-02 <abderrahmanebenaissa740@gmail.com>
2024-02-22 20:48:48 +0100

    Add code to define and display workspace variables

---
+++ Intro/problem1.m
@@ -0,0 +1,12 @@
+% define variables
+a = 2;
+A = [1 2; 3 4];
+T = 'stable';
+
+% Display workspace varaibles
+disp('Varaibles in workspace: ');
+who
+
+% Display detailed information about workspace varaibles
+disp('Detailed information about workspace varaibles: ');
+whos
