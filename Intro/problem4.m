066056c1d2d45379d56fab1e0affc14e98183ca2
abdulrahman-02 <abderrahmanebenaissa740@gmail.com>
2024-02-23 22:47:15 +0100

    Add calculation of A and C in problem4.m

---
+++ Intro/problem4.m
@@ -0,0 +1,5 @@
+% Calculate A using the given formula
+A = (exp((x*sin(x))/(x^2-1)))*abs((x^2-1)*(sqrt(x^(sin(log(abs(x-1))))))^abs(x^3-5));
+
+% Calculate C using logical operators
+C = (a & b) | (a | b);
