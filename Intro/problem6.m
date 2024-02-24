15b6315457238722bb124256a838de4fdbe48bb6
abdulrahman-02 <abderrahmanebenaissa740@gmail.com>
2024-02-24 21:19:46 +0100

    Fix command mistakes in problem6.m

---
+++ Intro/problem6.m
@@ -0,0 +1,27 @@
+% correct command mistakes
+% A = [1 2;4 5 6];
+A = [1 2 3;4 5 6];
+% V = [1 2 3;
+V = [1 2 3];
+% Net - cost = 15; correct this
+Net_cost = 15;
+% X01 = 10;
+X01 = 10;
+% 2pay = 3.5;
+pay2 = 3.5;
+%[n,m = size(A);
+[n,m] = size(A);
+% k = det(A;
+k = det(A);
+% X 9 = 16;
+X9 = 16;
+% For = 13;
+For = 13;
+% for = sin(pi);
+for1 = sin(pi);
+% Y = eps*cos(Pi);
+Y = eps*cos(Pi);
+% M = (2 > 3) & (5=3);
+M = (2 > 3) & (5==3);
+
+
